862961a4c134ebda22ff07d9d795ca13065024cf
Sergii Bogush <sergbogush@gmail.com>
2022-01-18 10:31:48 +0200

    add Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,37 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "./store/todoSlice";
+
 import TodoList from "./components/TodoList";
+import InputField from "./components/InputField";
 
 import "./App.css";
 function App() {
-  const [todos, setTodos] = useState([]);
   const [text, setText] = useState();
+  const dispatch = useDispatch();
 
-  const addTodo = () => {
-    if (text.trim().length) {
-      setTodos([
-        ...todos,
-        {
-          id: new Date().toISOString(),
-          text,
-          completed: false,
-        },
-      ]);
-      setText("");
-    }
-  };
+  const addTask = () => dispatch(addTodo(text));
 
   const toggleTodoComplete = (todoId) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id !== todoId) return todo;
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-
-        todo.completed = !todo.completed;
-      })
-    );
+    // setTodos(
+    //   todos.map((todo) => {
+    //     if (todo.id !== todoId) return todo;
+    //     return {
+    //       ...todo,
+    //       completed: !todo.completed,
+    //     };
+    //   })
+    // );
   };
 
   const removeTodo = (todoId) => {
-    setTodos(todos.filter((todo) => todo.id !== todoId));
+    // setTodos(todos.filter((todo) => todo.id !== todoId));
   };
 
   return (
     <div className="App">
-     
-      <TodoList
-        todos={todos}
-        toggleTodoComplete={toggleTodoComplete}
-        removeTodo={removeTodo}
-      />
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,13 @@
+import { useSelector } from "react-redux";
+
 import TodoItem from "./TodoItem";
 
-const TodoList = ({ todos, removeTodo, toggleTodoComplete }) => {
+const TodoList = () => {
+  const todos = useSelector((state) => state.todos.todos);
   return (
     <ul>
       {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          {...todo}
-          toggleTodoComplete={toggleTodoComplete}
-          removeTodo={removeTodo}
-        />
+        <TodoItem key={todo.id} {...todo} />
       ))}
     </ul>
   );
